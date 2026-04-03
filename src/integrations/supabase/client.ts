ad044fd7d20f8979b2581989b47f0e9f4cb2a44f
FenixSoulAr <fenixsoular@gmail.com>
2026-04-03 19:32:35 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://rckpejobuhbupmxlxnit.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_hgZWbqdqPeReWSy_0CX2_g_Xji1zzYJ";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
